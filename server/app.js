79d45b27d5301017384a49a9406a2e4aeb211559
loshaless <loshaless@gmail.com>
2021-05-28 00:06:03 +0700

    done error handler dan connect router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 var cors = require('cors')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const router = require('./routes/index')
+const errorHandler = require('./middleware/errorHandler')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use('/', router)
+app.use('/', errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
