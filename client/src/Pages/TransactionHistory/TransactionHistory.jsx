7dca49424de1dbd02005cc280f429d6d057a4411
Indra Akhmad <indraahmadfirdaus@gmail.com>
2021-05-30 21:48:27 +0700

    initial

---
+++ client/src/Pages/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+const TransactionHistory = () => {
+  return (
+    <div>
+      <p>Transaction History</p>
+    </div>
+  );
+};
+
+export default TransactionHistory;
