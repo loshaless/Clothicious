6355885704b3ae047c161e5c29467f4449977b03
Indra Akhmad <indraahmadfirdaus@gmail.com>
2021-05-30 21:31:46 +0700

    waiting for copywriting

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import ExploreProducts from "./Pages/ExploreProducts/ExploreProducts.jsx"
 import WomanProducts from "./Pages/ExploreProducts/WomanProducts.jsx"
 import ManProducts from "./Pages/ExploreProducts/ManProducts.jsx"
 import Details from "./Pages/Details/Details.jsx"
+import SuccessPage from "./Pages/SuccessPage/SuccessPage.jsx"
 import Navbar from "./Components/Navbar.jsx"
 import {
   BrowserRouter as Router,
@@ -46,6 +47,9 @@ function App() {
           </Route>
           <Route path="/browse/details/:id">
             <Details />
+          </Route>
+          <Route path="/success">
+            <SuccessPage />
           </Route>
         </Switch>
       </Router>
---
+++ client/src/Pages/SuccessPage/SuccessPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
+const SuccessPage = () => {
+  const history = useHistory();
+
+  function handleOnClickBrowseMore() {
+    history.push("/browse");
+  }
+  return (
+    <Flex
+      minH="90vh"
+      bg="mainColor.bg"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDirection="column"
+        h="60vh"
+        alignItems="center"
+        justifyContent="center"
+        spacing="5"
+      >
+        <Text
+          color="mainColor.fontColor"
+          fontSize="2xl"
+          fontWeight="bold"
+          letterSpacing="widest"
+        >
+          Transaction Success
+        </Text>
+        <Text>Kata kata here</Text>
+        <Button
+          w="100%"
+          borderRadius="0"
+          bg="black"
+          color="white"
+          colorScheme="black"
+          onClick={handleOnClickBrowseMore}
+        >
+          Browse More
+        </Button>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default SuccessPage;
