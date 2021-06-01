8936b4d2d25be3e68b96fff550716a5c69fa0592
Indra Akhmad <indraahmadfirdaus@gmail.com>
2021-06-01 13:12:20 +0700

    loading page & upload Product re-merge

---
+++ client/src/Pages/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Navbar from "../../Components/Navbar";
+import { useHistory } from "react-router-dom";
+import { Flex, Text, Button, Stack } from "@chakra-ui/react";
+const LoadingPage = () => {
+  const history = useHistory();
+  function handleOnClickTakeMeHome() {
+    history.push("/");
+  }
+  return (
+    <>
+      <Navbar />
+      <Flex
+        minH="90vh"
+        bg="mainColor.bg"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Stack
+          flexDirection="column"
+          h="60vh"
+          alignItems="center"
+          justifyContent="center"
+          spacing="5"
+        >
+          <Text
+            color="mainColor.fontColor"
+            fontSize="2xl"
+            fontWeight="bold"
+            letterSpacing="widest"
+          >
+            Your Page is Loading
+          </Text>
+          <Text>Please Wait ...</Text>
+        </Stack>
+      </Flex>
+    </>
+  );
+};
+
+export default LoadingPage;
