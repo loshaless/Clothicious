f7615a804da19477860ce1504a1424b6496d1d74
rizqizulfian <rizqizulfian@gmail.com>
2021-06-01 21:04:09 +0700

    seeder product

--- server/seeders/20210601094556-UsersSeeder.js
+++ server/seeders/20210601094556-UsersSeeder.js
@@ -1,8 +1,34 @@
 'use strict';
 const User = require('./dummydata/user.json')
+const axios = require('axios')
 const { hash } = require('../helper/bcrypt')
 
-User.forEach(el => el.password = hash(el.password))
+User.forEach((el, i) => {
+  const password = hash(el.password) 
+  el.password = password
+  
+  // const data = {
+  //   "username": el.username,
+  //   "secret": password.substring(0, 5),
+  //   "avatar": "https://i.pravatar.cc/300"
+  // }
+  // console.log('asdasd')
+  
+  // axios({
+  //   method: 'post',
+  //   url: 'https://api.chatengine.io/users/',
+  //   headers: {
+  //     'PRIVATE-KEY': '0cf963af-f223-46a4-b33b-f279bec65c17'
+  //   },
+  //   data: data
+  // })
+  // .then(response => {
+  //   console.log('oke', i)
+  // })
+  // .catch(error => {
+  //   console.log(error, 'error chatengine di model user');
+  // });
+})
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
